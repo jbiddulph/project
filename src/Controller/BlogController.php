52e14d845529b4f55cddf936d2579607f4b46ca4
John Biddulph <john.mbiddulph@gmail.com>
2019-06-05 21:32:45 +0100

    now its inserting data YAY

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,71 +1,77 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: telematicsdataservices
- * Date: 2019-06-04
- * Time: 22:12
- */
-
 namespace App\Controller;
-
-
+use App\Entity\BlogPost;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Serializer\Serializer;
 /**
  * @Route("/blog")
  */
 class BlogController extends AbstractController
 {
-    private const POSTS = [
-        [
-            'id' => 1,
-            'slug' => 'hello-world',
-            'title' => 'Hello World'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'hello-world2',
-            'title' => 'Hello World 2'
-        ],
-        [
-            'id' => 3,
-            'slug' => 'hello-world3',
-            'title' => 'Hello World 3'
-        ]
-
-    ];
-
     /**
-     * @Route("/{page}", name="blog_list", defaults={"page": 5})
+     * @Route("/{page}", name="blog_list", defaults={"page": 5}, requirements={"page"="\d+"})
      */
-    public function list($page = 1, Request $request) {
+    public function list($page = 1, Request $request)
+    {
         $limit = $request->get('limit', 10);
-        return $this->json([
-            'page' => $page,
-            'limit' => $limit,
-            'data' => array_map(function ($item){
-                return $this->generateUrl('blog_by_slug', ['slug' => $item['slug']]);
-            }, self::POSTS)
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="blog_by_id")
-     */
-    public function post($id) {
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $items = $repository->findAll();
         return $this->json(
-          array_search($id, array_column(self::POSTS, 'id'))
+            [
+                'page' => $page,
+                'limit' => $limit,
+                'data' => array_map(function (BlogPost $item) {
+                    return $this->generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
+                }, $items)
+            ]
         );
     }
-
     /**
-     * @Route("/{slug}", name="blog_by_slug")
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
+     * @ParamConverter("post", class="App:BlogPost")
      */
-    public function post_by_slug($slug) {
-        return $this->json(
-            array_search($slug, array_column(self::POSTS, 'slug'))
-        );
+    public function post($post)
+    {
+        // It's the same as doing find($id) on repository
+        return $this->json($post);
+    }
+    /**
+     * @Route("/post/{slug}", name="blog_by_slug", methods={"GET"})
+     * The below annotation is not required when $post is typehinted with BlogPost
+     * and route parameter name matches any field on the BlogPost entity
+     * @ParamConverter("post", class="App:BlogPost", options={"mapping": {"slug": "slug"}})
+     */
+    public function postBySlug(BlogPost $post)
+    {
+        // Same as doing findOneBy(['slug' => contents of {slug}])
+        return $this->json($post);
+    }
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request)
+    {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+        return $this->json($blogPost);
+    }
+    /**
+     * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+     */
+    public function delete(BlogPost $post)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($post);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
